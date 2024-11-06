5cd9aea64d7fdc39f6a82c2626954032469117f5
Imran Ali Namazi <team@yieldmore.org>
2024-11-06 07:42:43 +0530

    updates, footer, footer bgd color, 2x logo - make it look bigger

---
+++ code/footer-content.php
@@ -0,0 +1,21 @@
+<?php
+$homeUrl = homeUrl();
+$logoUrl =  logoUrl();
+$logoRel =  logoRel();
+?>
+<div id="footer-content" class="footer-bgd" style="margin-top: 30px;">
+	<div class="container">
+		<?php runCode('engage'); ?>
+		<section id="footer-message">
+			<a href="<?php echo $homeUrl;?>"><img src="<?php echo $logoUrl; ?><?php echo $logoRel; ?>-logo@2x.png" class="img-fluid img-max-500" alt="<?php echo am_var('name'); ?>" /></a><br />
+			<u><?php echo am_var('name'); ?></u> &mdash;
+			<?php renderMarkdown(am_var_or('footer-message', ''), ['strip-paragraph-tag' => true]); ?>
+			<hr />
+			<div class="social-links"><?php foreach(am_var('social') as $item) { ?>
+				<a target="_blank" href="<?php echo $item['link']; ?>" title="<?php echo isset($item['name']) ? $item['name'] : $item['type']; ?>" class="<?php echo $item['type']; ?>"><i class="icofont-<?php echo $item['type']; ?>"></i></a><?php } ?>
+			</div>
+		</section>
+		<?php load_amadeus_module('share'); ?>
+	</div>
+</div>
+
