b6b9c96ceec0bbfdb42accea49289764dba63b5f
Imran N <team@yieldmore.org>
2024-12-30 09:18:23 +0530

    people from md files

--- content/people.php
+++ content/people.php
@@ -11,30 +11,38 @@ $lastGroup = false;
 
 $urlBase = am_var('url') . 'assets/people/';
 $folBase = SITEPATH . '/assets/people/';
+$aboutBase = SITEPATH . '/content/people/';
 
 foreach ($sheet->rows as $item) {
+	$name = $item[$cols['name']];
+	if (!$name) continue;
+
 	$group = $item[$cols['group']];
 	if ($lastGroup != $group) {
 		$safeGroup = urlize($group);
 		sectionId($safeGroup);
 		echo '<a name="' . $safeGroup . '"></a>';
-		echo '<h1>' . $group . '</h1>';
+		echo '<h1 style="text-align: center;">' . $group . '</h1>';
 		section('end');
 	}
 	$lastGroup = $group;
 
-	$name = $item[$cols['name']];
 	sectionId($safeName = urlize($name));
 	echo '<a name="' . $safeName . '"></a>';
 	echo '<h2>' . $name . '</h2>';
 	echo '<div>';
 
 	if (disk_file_exists($folBase . $safeName . '.jpg'))
-		echo replaceItems('<img class="bordered-image img-right img-max-300" src="%src%" alt="%name" />', ['src' => $urlBase . $safeName . '.jpg', 'name' => $name], '%') . am_var('2nl');
+		echo replaceItems('<img class="bordered-image img-right img-max-300" src="%src%" alt="%name" />', [
+			'src' => $urlBase . $safeName . '.jpg',
+			'name' => $name], '%') . am_var('2nl');
 
-	$about = simplify_encoding($item[$cols['about']]);
-	$about = str_replace('|', '<br />', $about);
-	renderMarkdown($about);
-	echo '</div>';
+	echo '	<h3><i>' . $item[$cols['role']] . '</i></h3>' . am_var('nl');
+
+	$about = $aboutBase . $safeName . '.md';
+	if (disk_file_exists($about))
+		renderMarkdown($about);
+
+		echo '</div>';
 	section('end');
 }
